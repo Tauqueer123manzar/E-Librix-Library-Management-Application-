5b667f2da7f779f9e697c770a3e6bc05077315ae
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-03-21 02:48:43 +0530

    Initial commit of the frontend and backend

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const app=express();
+
+const PORT=process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
+
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-center text-red-500 font-bold'>Library Management System</h1>
+      <h2 className='text-2xl text-center mt-5'>This is a library management system</h2>
+    </div>
+  )
+}
+
+export default App
