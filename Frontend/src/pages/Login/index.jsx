6f9b1ee5c3cebad5ecb273594ce239d967093075
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-03-27 20:46:27 +0530

    Login page added

--- Frontend/src/pages/Login/index.jsx
+++ Frontend/src/pages/Login/index.jsx
@@ -4,8 +4,23 @@ import Footer from '../../components/Footer'
 const index = () => {
   return (
     <>
-        <div className='mx-auto bg-white h-screen'>
-           <h1 className='text-3xl font-bold text-black mt-25 text-center'>User Login</h1>
+        <div className='mx-auto bg-white h-screen flex flex-col m-5 p-4'>
+           <h3 className='text-2xl  text-blue-500 mt-25 text-center' style={{fontFamily:"initial"}}>Welcome Back! Log in to Your Account</h3>
+           <div className='flex flex-col bg-white w-full md:w-1/3 h-[450px] md:h-1/2 mx-auto mt-10 shadow-2xl px-10 rounded-lg'>
+            <h2 className='text-center text-black text-2xl mt-4'>User Login</h2>
+             <input 
+              type='text'
+              placeholder='Enter Email'
+              className='border border-gray-300 rounded-md p-2 w-full mt-10 '
+              />
+              <input
+              type='password'
+              placeholder='Enter Password'
+              className='border border-gray-300 rounded-md p-2 w-full mt-5 mx-auto'
+              />
+              <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 mx-auto w-full cursor-pointer'>Login</button>
+              <p className='text-center mt-5 gap-2 text-blue'>If you don't have an account then please Signup?  <a href='/signup' className='text-blue-500 hover:text-blue-700 gap-4'>  Signup</a></p>
+           </div>
         </div>
         <div className='mt-2'>
             <Footer/>
