12d7900c571acba88d76eb1dcac23b209c7f97d1
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-04-14 00:16:57 +0530

    authMiddleware working is going on

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const catchAsyncErrors=require("./catchAsyncErrors");
+const ErrorHandler=require("./ErrorHandler");
+const User=require("../models/userModel");
+const jwt=require("jsonwebtoken");
+
+// This middleware checks if the user is authenticated by verifying the JWT token
+exports.isAuthenticated=catchAsyncErrors(async(req,res,next)=>{
+    const {token}=req.cookies;
+    if(!token){
+        return next(new ErrorHandler("user is not Authenticated",401));
+    };
+    const decodedData=jwt.verify(token,process.env.JWT_SECRET_KEY);
+    console.log(decodedData);
+    req.user=await User.findById(decodedData.id);
+    next();
+});
+
